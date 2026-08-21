5c6ea48c846684ba965fe58d372bf7aa6ef7dc73
Mqid <68720462+Mqsirrel@users.noreply.github.com>
2026-08-21 05:29:29 +0300

    refactor: compose responsive presentation layers

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import './styles/desktop-presentation.css';
+import './styles/tablet-presentation.css';
 import './styles/mobile-presentation.css';
 import { ScheduleMakerApp } from './application/ScheduleMakerApp.js';
 
