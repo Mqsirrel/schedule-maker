c006d0d2e3d0fd06cdb0916e1a82a694950bb376
Mqid <68720462+Mqsirrel@users.noreply.github.com>
2026-08-21 05:21:53 +0300

    refactor: introduce application state container

---
+++ src/application/AppState.js
@@ -0,0 +1,67 @@
+export class AppState {
+  constructor(initial = {}) {
+    this._state = {
+      timetableSections: [],
+      wantedCourseGroups: [],
+      allGeneratedSchedules: [],
+      filteredSchedules: [],
+      currentScheduleIndex: 0,
+      currentView: 'calendar',
+      ...initial
+    };
+  }
+
+  get(key) {
+    return this._state[key];
+  }
+
+  set(key, value) {
+    this._state[key] = value;
+    return value;
+  }
+
+  patch(values) {
+    Object.assign(this._state, values);
+  }
+
+  snapshot() {
+    return { ...this._state };
+  }
+
+  resetResults() {
+    this.patch({
+      allGeneratedSchedules: [],
+      filteredSchedules: [],
+      currentScheduleIndex: 0
+    });
+  }
+
+  setGeneratedSchedules(schedules) {
+    this.patch({
+      allGeneratedSchedules: schedules,
+      filteredSchedules: schedules,
+      currentScheduleIndex: 0
+    });
+  }
+
+  setFilteredSchedules(schedules) {
+    this.patch({ filteredSchedules: schedules, currentScheduleIndex: 0 });
+  }
+
+  navigateSchedule(delta) {
+    const schedules = this.get('filteredSchedules');
+    const current = this.get('currentScheduleIndex');
+    const next = current + delta;
+
+    if (next >= 0 && next < schedules.length) {
+      this.set('currentScheduleIndex', next);
+      return true;
+    }
+
+    return false;
+  }
+
+  getCurrentSchedule() {
+    return this.get('filteredSchedules')[this.get('currentScheduleIndex')];
+  }
+}
